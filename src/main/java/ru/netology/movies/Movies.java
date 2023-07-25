2f30691493f5b81e2535d511792870533cbff41f
Alena Kuimova <alvikui@yandex.ru>
2023-07-25 11:14:04 +0600

    PosterManagerMovies

---
+++ src/main/java/ru/netology/movies/Movies.java
@@ -0,0 +1,15 @@
+package ru.netology.movies;
+
+public class Movies {
+    public static void main(String[] args) {
+        String[] names = {
+                "Бладшот",
+                "Вперед",
+                "Отель <Белград>",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли.Мировой тур",
+                "Номер один"
+        };
+    }
+}
